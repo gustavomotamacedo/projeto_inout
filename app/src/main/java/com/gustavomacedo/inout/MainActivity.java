3a7cbca3cdba600e5197282058d8e6670a93e435
Gustavo Macedo <91705195+gustavomotamacedo@users.noreply.github.com>
2024-09-19 15:51:12 -0300

    função de colocar os dados da tabela na tela

--- app/src/main/java/com/gustavomacedo/inout/MainActivity.java
+++ app/src/main/java/com/gustavomacedo/inout/MainActivity.java
@@ -1,14 +1,25 @@
 package com.gustavomacedo.inout;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView alunosView;
+    private DbHelper dbHelper;
+    private ArrayList<String> alunosNome, alunosRGM, alunosData, alunosHoraEntrada, alunosHoraSaida;
+    private AlunoAdapter alunoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        alunosView = findViewById(R.id.alunosView);
+        dbHelper = new DbHelper(MainActivity.this);
+
+        alunosNome = new ArrayList<>();
+        alunosRGM = new ArrayList<>();
+        alunosData = new ArrayList<>();
+        alunosHoraEntrada = new ArrayList<>();
+        alunosHoraSaida = new ArrayList<>();
+
+        guardarDadosNosArrays();
+
+        alunoAdapter = new AlunoAdapter(MainActivity.this, alunosNome, alunosRGM, alunosData, alunosHoraEntrada, alunosHoraSaida);
+
+        alunosView.setAdapter(alunoAdapter);
+        alunosView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+    }
+
+    void guardarDadosNosArrays() {
+        Cursor cursor = dbHelper.readAllData();
+        if (cursor.getCount() == 0) {
+            Toast.makeText(getApplicationContext(), "Sem dados na tabela", Toast.LENGTH_SHORT).show();
+        } else {
+            while(cursor.moveToNext()) {
+                alunosNome.add(cursor.getString(0));
+                alunosRGM.add(cursor.getString(1));
+                alunosData.add(cursor.getString(2));
+                alunosHoraEntrada.add(cursor.getString(3));
+                alunosHoraSaida.add(cursor.getString(4));
+            }
+        }
     }
 }
