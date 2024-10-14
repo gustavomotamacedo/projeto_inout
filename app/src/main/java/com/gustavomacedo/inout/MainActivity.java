a9a0aabba02fbfbaf78058f769dc031d44db6844
gustavomotamacedo <mota.macedo05@gmail.com>
2024-10-14 10:58:41 -0300

    Lendo alunos e eventos pelo arquivo CSV

--- app/src/main/java/com/gustavomacedo/inout/MainActivity.java
+++ app/src/main/java/com/gustavomacedo/inout/MainActivity.java
@@ -1,10 +1,12 @@
 package com.gustavomacedo.inout;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.ActionBar;
@@ -13,9 +15,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtInOut;
+    private final String CSV_PATH_ALUNOS = "/data/data/com.gustavomacedo.inout/files/alunos.csv" ;
+    private final String CSV_PATH_EVENTOS = "/data/data/com.gustavomacedo.inout/files/eventos.csv" ;
+    private List<AlunoBean> alunoBeanList;
+    private List<EventoBean> eventoBeanList;
+    private DbHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,43 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.hide();
+
+        try {
+            //noinspection unchecked,rawtypes
+            alunoBeanList = new CsvToBeanBuilder(new FileReader(CSV_PATH_ALUNOS)).withType(AlunoBean.class).build().parse();
+            //noinspection unchecked,rawtypes
+            eventoBeanList = new CsvToBeanBuilder(new FileReader(CSV_PATH_EVENTOS)).withType(EventoBean.class).build().parse();
+
+            dbHelper = new DbHelper(this);
+
+            Cursor eventos = dbHelper.lerTodosOsEventos();
+            Cursor alunos = dbHelper.lerTodosOsAlunos();
+
+            if (eventos.getCount() == 0) {
+                // Tabela de eventos vazia
+                for (int i = 0; i < eventoBeanList.size(); i++) {
+                    dbHelper.adcEvento(eventoBeanList.get(i).getNome());
+                }
+            }
+            else {
+                // Tabela de eventos com valores
+                Toast.makeText(this, "Eventos já adicionados", Toast.LENGTH_SHORT).show();
+            }
+
+            if (alunos.getCount() == 0) {
+                // Tabela de alunos vazia
+                for (int i = 0; i < alunoBeanList.size(); i++) {
+                    AlunoBean alunoAux = alunoBeanList.get(i);
+                    dbHelper.adcAluno(alunoAux.getNome(), Integer.parseInt(alunoAux.getRgm()), Integer.parseInt(alunoAux.getIdEvento()), alunoAux.getData());
+                }
+            } else {
+                // Tabela de alunos com valores
+                Toast.makeText(this, "Alunos já adicionados", Toast.LENGTH_SHORT).show();
+            }
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
         txtInOut = findViewById(R.id.txtInOut);
 
